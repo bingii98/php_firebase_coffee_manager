3f07ad9e2bad6775c4336a66c198c9cd626a8af5
Nguyễn Trường Giang <44118083+bingii98@users.noreply.github.com>
2020-05-14 05:02:05 +0700

    Upload code

--- manager.php
+++ manager.php
@@ -12,6 +12,14 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 </head>
 <body>
+<section>
+    <div class="overlay-notify">
+        <div class="box">
+            <button class="btn-close"><i class="fa fa-times" aria-hidden="true"></i></button>
+        </div>
+        <div class="bg-overlay"></div>
+    </div>
+</section>
 <header class="navbar-manager -bg-darkblue">
     <div class="container" style="display: flex;">
         <div class="panel-tab">
@@ -26,7 +34,8 @@
         </div>
         <div class="status-tab">
             <ul>
-                <li><i class="fa fa-coffee" aria-hidden="true"></i>&nbsp;&nbsp;<p>09</p> <span>/</span> <p>34</p><label>Active table</label>
+                <li><i class="fa fa-coffee" aria-hidden="true"></i>&nbsp;&nbsp;<p>09</p> <span>/</span>
+                    <p>34</p><label>Active table</label>
                 </li>
             </ul>
         </div>
@@ -51,19 +60,101 @@
         </div>
         <div class="content col-8">
             <ul class="card-box-desk">
-                <li class="card-list active"><lable>Bàn 1</lable></li>
-                <li class="card-list"><label>Bàn 2</label></li>
-                <li class="card-list"><label>Bàn 3</label></li>
-                <li class="card-list"><label>Bàn 4</label></li>
-                <li class="card-list"><label>Bàn 5</label></li>
-                <li class="card-list"><label>Bàn 6</label></li>
-                <li class="card-list"><label>Bàn 7</label></li>
-                <li class="card-list"><label>Bàn 8</label></li>
-                <li class="card-list"><label>Bàn 9</label></li>
-                <li class="card-list"><label>Bàn 10</label></li>
+                <li class="card-list active">
+                    <label>Bàn 1</label>
+                    <div class="status">
+                        <p><i class="fa fa-coffee" aria-hidden="true"></i> 6</p>
+                        <p><i class="fa fa-user" aria-hidden="true"></i> 6</p>
+                    </div>
+                    <button><i class="fa fa-eye" aria-hidden="true"></i> xem chi tiết</button>
+                </li>
+                <li class="card-list">
+                    <label>Bàn 2</label>
+                    <div class="status">
+                        <p><i class="fa fa-coffee" aria-hidden="true"></i> 6</p>
+                        <p><i class="fa fa-user" aria-hidden="true"></i> 6</p>
+                    </div>
+                    <button><i class="fa fa-eye" aria-hidden="true"></i> xem chi tiết</button>
+                </li>
+                <li class="card-list"><label>Bàn 3</label>
+                    <div class="status">
+                        <p><i class="fa fa-coffee" aria-hidden="true"></i> 6</p>
+                        <p><i class="fa fa-user" aria-hidden="true"></i> 6</p>
+                    </div>
+                    <button><i class="fa fa-eye" aria-hidden="true"></i> xem chi tiết</button>
+                </li>
+                <li class="card-list"><label>Bàn 4</label>
+                    <div class="status">
+                        <p><i class="fa fa-coffee" aria-hidden="true"></i> 6</p>
+                        <p><i class="fa fa-user" aria-hidden="true"></i> 6</p>
+                    </div>
+                    <button><i class="fa fa-eye" aria-hidden="true"></i> xem chi tiết</button>
+                </li>
+                <li class="card-list active"><label>Bàn 5</label>
+                    <div class="status">
+                        <p><i class="fa fa-coffee" aria-hidden="true"></i> 6</p>
+                        <p><i class="fa fa-user" aria-hidden="true"></i> 6</p>
+                    </div>
+                    <button><i class="fa fa-eye" aria-hidden="true"></i> xem chi tiết</button>
+                </li>
+                <li class="card-list"><label>Bàn 6</label>
+                    <div class="status">
+                        <p><i class="fa fa-coffee" aria-hidden="true"></i> 6</p>
+                        <p><i class="fa fa-user" aria-hidden="true"></i> 6</p>
+                    </div>
+                    <button><i class="fa fa-eye" aria-hidden="true"></i> xem chi tiết</button>
+                </li>
+                <li class="card-list active"><label>Bàn 7</label>
+                    <div class="status">
+                        <p><i class="fa fa-coffee" aria-hidden="true"></i> 6</p>
+                        <p><i class="fa fa-user" aria-hidden="true"></i> 6</p>
+                    </div>
+                    <button><i class="fa fa-eye" aria-hidden="true"></i> xem chi tiết</button>
+                </li>
+                <li class="card-list"><label>Bàn 8</label>
+                    <div class="status">
+                        <p><i class="fa fa-coffee" aria-hidden="true"></i> 6</p>
+                        <p><i class="fa fa-user" aria-hidden="true"></i> 6</p>
+                    </div>
+                    <button><i class="fa fa-eye" aria-hidden="true"></i> xem chi tiết</button>
+                </li>
+                <li class="card-list"><label>Bàn 9</label>
+                    <div class="status">
+                        <p><i class="fa fa-coffee" aria-hidden="true"></i> 6</p>
+                        <p><i class="fa fa-user" aria-hidden="true"></i> 6</p>
+                    </div>
+                    <button><i class="fa fa-eye" aria-hidden="true"></i> xem chi tiết</button>
+                </li>
+                <li class="card-list"><label>Bàn 10</label>
+                    <div class="status">
+                        <p><i class="fa fa-coffee" aria-hidden="true"></i> 6</p>
+                        <p><i class="fa fa-user" aria-hidden="true"></i> 6</p>
+                    </div>
+                    <button><i class="fa fa-eye" aria-hidden="true"></i> xem chi tiết</button>
+                </li>
             </ul>
         </div>
     </div>
 </section>
+<script src="public/asset/js/jquery-3.5.1.min.js"></script>
+<script !src="">
+    $(document).ready(function () {
+        $(".card-list").click(function () {
+            console.log($(".card-list .disable").length);
+            if($(this).hasClass("disable")){
+                $(".card-list").addClass("disable");
+                $(this).removeClass("disable");
+                $(this).removeClass("disable");
+            }else{
+                if($(".card-list.disable").length == 0){
+                    $(".card-list").addClass("disable");
+                    $(this).removeClass("disable");
+                }else{
+                    $(".card-list").removeClass("disable");
+                }
+            }
+        })
+    })
+</script>
 </body>
 </html>
